427b74aa97fc205f885787d000dfb6d813ffa4d7
Tom.Li <open222333@gmail.com>
2023-04-12 15:15:19 +0800

    20230412

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Bash-RenewSSL
-刷新ssl證書
+
+刷新ssl證書並傳送到目標主機
+
+```bash
+# CentOS
+yum install sshpass -y
+
+# 兩個月執行一次
+0 0 2 * * /bin/bash /path/to/renew_ssl $domain
+```
---
+++ renew_ssl.sh
@@ -0,0 +1,24 @@
+source `dirname -- "$0"`/.env
+
+# 參數1 指定域名
+if [[ $1 ]]; then
+	DOMAIN=$1
+	echo "DOMAIN: $DOMAIN"
+
+	if [[ $CLI_INI ]]; then
+		cd /etc/letsencrypt && certbot -c {$CLI_INI} renew --quiet --dns-cloudflare --force-renew --no-autorenew --cert-name $DOMAIN
+		git add .
+		git commit -m 刷新證書
+		git push
+	else
+		cd /etc/letsencrypt && certbot renew --quiet --dns-cloudflare --force-renew --no-autorenew --cert-name $DOMAIN
+		git add .
+		git commit -m 刷新證書
+		git push
+	fi
+
+	rsync -Pavr -e "sshpass -p$HOST_PASSWORD ssh" /etc/letsencrypt/archive/$DOMAIN root@$HOST_IP:/etc/letsencrypt/archive/$DOMAIN
+	rsync -Pavr -e "sshpass -p$HOST_PASSWORD ssh" /etc/letsencrypt/live/$DOMAIN root@$HOST_IP:/etc/letsencrypt/live/$DOMAIN
+else
+	echo "需指定參數 domain"
+fi
